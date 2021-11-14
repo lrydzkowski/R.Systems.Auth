4966cf6ee3d0e95c5df35e3f91e39f74316eb1dc
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2021-11-14 09:46:33 +0100

    build: remove R.Systems.Auth.Jwt project

--- R.Systems.Auth.WebApi/DependencyInjection/ServiceCollectionExtensions.cs
+++ R.Systems.Auth.WebApi/DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,11 +2,15 @@
 using Microsoft.Extensions.DependencyInjection;
 using R.Systems.Auth.Core.DependencyInjection;
 using R.Systems.Auth.WebApi.Interfaces;
-using R.Systems.Auth.Jwt.DependencyInjection;
 using R.Systems.Auth.WebApi.Services;
 using R.Systems.Auth.WebApi.Settings;
 using System.IO;
 using R.Systems.Auth.Db.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
 
 namespace R.Systems.Auth.WebApi.DependencyInjection
 {
@@ -17,7 +21,7 @@ namespace R.Systems.Auth.WebApi.DependencyInjection
             AddSettingsServices(services, configuration);
             services.AddScoped<IPrivateKeyLoader, PrivateKeyFileLoader>();
             services.AddScoped<UserService>();
-            services.AddScoped<AuthenticationService>();
+            services.AddScoped<Services.AuthenticationService>();
             services.AddDbServices(configuration["DbConnectionString"]);
             services.AddCoreServices();
             services.AddJwtServices(File.ReadAllText(configuration["Jwt:PublicKeyPemFilePath"]));
@@ -26,6 +30,33 @@ namespace R.Systems.Auth.WebApi.DependencyInjection
         private static void AddSettingsServices(IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.PropertyName));
+        }
+        public static void AddJwtServices(this IServiceCollection services, string publicKeyPem)
+        {
+            services.AddAuthentication(AddAuthenticationAction).AddJwtBearer(AddJwtBearerAction(publicKeyPem));
+        }
+
+        private static void AddAuthenticationAction(AuthenticationOptions options)
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }
+
+        private static Action<JwtBearerOptions> AddJwtBearerAction(string publicKeyPem)
+        {
+            return new Action<JwtBearerOptions>(options =>
+            {
+                RSA rsa = RSA.Create();
+                rsa.ImportFromPem(publicKeyPem.ToCharArray());
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = new RsaSecurityKey(rsa)
+                };
+            });
         }
     }
 }
