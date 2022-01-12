25782a28f91687eb20b9b382bd9c2c00f5b5622d
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-01-12 18:14:11 +0100

    build: add sql scripts for version 1.0.0-preview.2

---
+++ R.Systems.Auth.Infrastructure.Db/_Assets/SQL/v1.0.0-preview.2_down.sql
@@ -0,0 +1,33 @@
+﻿START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    ALTER TABLE "user"."user" DROP COLUMN last_incorrect_sign_in_date_time_utc;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    ALTER TABLE "user"."user" DROP COLUMN num_of_incorrect_sign_in;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    ALTER TABLE "user"."user" ALTER COLUMN refresh_token_expire_date_time_utc TYPE timestamp without time zone;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    DELETE FROM "__EFMigrationsHistory"
+    WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification';
+    END IF;
+END $EF$;
+COMMIT;
+
---
+++ R.Systems.Auth.Infrastructure.Db/_Assets/SQL/v1.0.0-preview.2_up.sql
@@ -0,0 +1,33 @@
+﻿START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    ALTER TABLE "user"."user" ALTER COLUMN refresh_token_expire_date_time_utc TYPE timestamp with time zone;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    ALTER TABLE "user"."user" ADD last_incorrect_sign_in_date_time_utc timestamp with time zone NULL;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    ALTER TABLE "user"."user" ADD num_of_incorrect_sign_in integer NULL;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220110172146_incorrect_sign_in_verification') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20220110172146_incorrect_sign_in_verification', '6.0.0');
+    END IF;
+END $EF$;
+COMMIT;
+
