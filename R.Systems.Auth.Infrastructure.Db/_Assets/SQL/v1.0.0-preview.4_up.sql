cad075592a46c44865e76894aa563f5d0cef06ee
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-02-06 10:24:41 +0100

    feat: remove test table

---
+++ R.Systems.Auth.Infrastructure.Db/_Assets/SQL/v1.0.0-preview.4_up.sql
@@ -0,0 +1,19 @@
+﻿START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220206092253_remove_test_table') THEN
+    DROP TABLE "user".test;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220206092253_remove_test_table') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20220206092253_remove_test_table', '6.0.0');
+    END IF;
+END $EF$;
+COMMIT;
+
